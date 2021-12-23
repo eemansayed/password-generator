1d4ac3f30e6d15d42550423421088ad129fc0f4a
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-23 11:10:05 +0000

    add function called generatePassword

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
-  symbol: getRandomSymbols,
+  symbol: getRandomSymbol,
 };
 
 generateEl.addEventListener("click", () => {
@@ -30,6 +30,28 @@ generateEl.addEventListener("click", () => {
   );
 });
 
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -42,7 +64,7 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function getRandomSymbols() {
+function getRandomSymbol() {
   const symbols = "!@#$%^&*()[]=<>/,.";
   return (symbols = [Math.floor(Math.random() * symbols.length)]);
 }
